c6f2aa733f660706b53cb48ff35a6acfec3a7303
xristina <ckaralka@csd.auth.gr>
2023-12-30 01:34:35 +0200

    update slides notes

--- RStarTreeScala/src/main/scala/Util/Constants.scala
+++ RStarTreeScala/src/main/scala/Util/Constants.scala
@@ -5,5 +5,5 @@ object Constants {
   val sizeOfDouble: Int = 8
   val sizeOfInt: Int = 4
   val ONE_MM: Double = 0.001
-  val UP_LIMIT: Int = 32768 //bytes = 32 KB
+  val UP_LIMIT: Int = 2048 //32768 //bytes = 32 KB
 }
