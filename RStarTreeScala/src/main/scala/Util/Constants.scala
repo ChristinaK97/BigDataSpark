d51d9dd7bd56604870a99ca8c0327e42765917ce
xristina <ckaralka@csd.auth.gr>
2024-01-22 05:52:03 +0200

    update node capacity

--- RStarTreeScala/src/main/scala/Util/Constants.scala
+++ RStarTreeScala/src/main/scala/Util/Constants.scala
@@ -4,9 +4,14 @@ object Constants {
   var N: Int = -1
   val sizeOfDouble: Int = 8
   val sizeOfInt: Int = 4
-  val ONE_MM: Double = 0.001
-  val UP_LIMIT: Int = 2048 //8192 //2048 //32768 //bytes
-  val RESET_TREES = true
+  val ONE_MM: Double = 0.00001
+
+  //8192 // 2048 // // 32768 //8192 //2048 // 16384// //bytes
+  val SMALL_SET_LIMIT = 8192
+  val LARGE_SET_LIMIT = 16384
+  val UP_LIMIT: Int = SMALL_SET_LIMIT
+
+  val RESET_TREES: Boolean = true
   val DEBUG: Boolean = false
   val DEBUG_SKY = false
   val DEBUG_TOPK = false
